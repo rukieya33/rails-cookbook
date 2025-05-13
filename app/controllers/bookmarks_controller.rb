b88c99d61c68c4d8cde78a3f322941510839a717
Rukieya Ainoo <rukieya.a@gmail.com>
2025-05-13 11:30:28 +0100

    bookmark controller, view created and create bookmark, delete bookmark working

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,36 @@
+class BookmarksController < ApplicationController
+
+  def index
+    @bookmarks = Bookmark.all
+    redirect_to bookmark_path(@bookmarks)
+  end
+  def new
+    @category = Category.find(params[:category_id])
+    @bookmark = Bookmark.new
+  end
+
+  def create
+
+    @category = Category.find(params[:category_id])
+    @bookmark = Bookmark.create(bookmark_params)
+    @bookmark.category = @category
+    if @bookmark.save
+      redirect_to category_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+ def destroy
+    @bookmark = Bookmark.find(params[:id])
+    @bookmark.destroy
+    redirect_to category_path(@bookmark.category), status: :see_other
+  end
+
+  private
+
+  def bookmark_params
+    params.require(:bookmark).permit(:comment, :category_id, :recipe_id)
+  end
+
+end
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,36 @@
 class CategoriesController < ApplicationController
+    #before_action :set_category, only: %i[new create]
   def index
     @categories = Category.all
     render "categories/index"
   end
 
-  def new
-    @categories = Category.create(name: params[:name])
-    redirect_to categories_path(@categories)
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path(@category)
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
+
+  def new
+    @category = Category.new
+    render "categories/new"
+  end
+
+  def show
+    @category = Category.find(params[:id])
+  end
+
+  private
+
+=begin def set_category
+    @category = Category.find(params[:category_id])
+  end
+=end
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
